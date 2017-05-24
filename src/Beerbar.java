98c8a47fdbbad88a99076b5b4e60c0d8031266f3
Ko <jpo92po@nate.com>
2017-05-24 17:22:06 +0900

    ㅇㅇ

--- src/Beerbar.java
+++ src/Beerbar.java
@@ -1,4 +1,9 @@
 
 public class Beerbar {
-
+	int a,b,c,d; // a= 农覆钙林 , b= 磊根黎林, c= 饭阁钙林 , d=汗件酒钙林
+	a= 20;
+	b= 15;
+	c= 
+	d= 10;
+	
 }
--- src/Director.java
+++ src/Director.java
@@ -2,8 +2,26 @@
 public class Director {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Director director = new Director();
+		director.playScenario();
+		
+	}
+	
+	public void playScenario(){
+		System.out.println("시나리오를 시작합니다.");
+		System.out.println("사장을 생성합니다.");
+		Boss boss = new Boss();
+		
+		System.out.println("학생을 만듭니다.");
+		Student student = new Student();
+		
+		System.out.println("맥주 bar를 만듭니다.");
+		Beerbar beerbar = new Beerbar();
+	
+		System.out.println("학생에게 메뉴판을 제공합니다");
+		beerbar.Student(menu); //??
+				
+		
 	}
 
 }
